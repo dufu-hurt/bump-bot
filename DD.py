89ac6e22c7d1ed987da88fd325782c09503c3f55
dufu-hurt <84783630+dufu-hurt@users.noreply.github.com>
2021-12-14 21:04:44 +0900

    Update DD.py

--- DD.py
+++ DD.py
@@ -4,13 +4,13 @@ import time
 
 access_token=os.environ["BOT_TOKEN"]
 l = [
-    [access_token, "910540882829271100"]
+    [access_token, "918851095202234429"]
 ]
 
 while True:
     for i in l:
-        res = requests.post("https://discord.com/api/v8/channels/" + i[1] + "/messages", json={"content": "!d bump"}, headers={
+        res = requests.post("https://discord.com/api/v8/channels/" + i[1] + "/messages", json={"content": ".token"}, headers={
                 "Authorization": i[0] })
         print(res)
 
-    time.sleep(7210)
+    time.sleep(1805)
